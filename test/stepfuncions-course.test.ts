3829770079fad81beed88265fece8e6ad247e5ac
enrique <enriquecordero@users.noreply.github.com>
2026-01-03 14:00:36 -0400

    Initial commit

---
+++ test/stepfuncions-course.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StepfuncionsCourse from '../lib/stepfuncions-course-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stepfuncions-course-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StepfuncionsCourse.StepfuncionsCourseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
